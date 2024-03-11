db53b7c678038d049396e3f99e36e06c8354be80
JJinse <154294947+JJinse123@users.noreply.github.com>
2024-03-11 21:18:00 +0900

    1_4_파일분할예제

---
+++ 1_4_question_파일분할/1_4_question_파일분할/SimpleFun.h
@@ -0,0 +1,10 @@
+namespace BestComImpl
+{
+	void SimpleFunc(void);
+}
+namespace ProgComImpl
+{
+	void SimpleFunc(void);
+}
+
+//헤더파일에는 함수의 선언부분 작성
---
+++ 1_4_question_파일분할/1_4_question_파일분할/main.cpp
@@ -0,0 +1,9 @@
+#include "SimpleFun.h"
+
+int main(void){
+	BestComImpl::SimpleFunc();
+	ProgComImpl::SimpleFunc();
+	return 0;
+}
+
+//SimpleFunc() 사용을 위한 헤더파일 참조
